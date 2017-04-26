06ba3540b1c667d79aa35c651481255e6a993f9e
Jeremy Morel <jeremy.morel@opendatasoft.com>
2017-04-26 14:07:39 +0200

    load remote data and use vue

--- logic.js
+++ logic.js
@@ -1,13 +1,80 @@
 'use strict';
 
+var buildGetUrl = function (url, params) {
+    var encodedParams = Object.keys(params).reduce(function (previousEncodedParams, paramName) {
+        var paramValues = params[paramName];
+        if (!Array.isArray(paramValues)) {
+            paramValues = [paramValues];
+        }
+        var encodedParam = paramValues.map(function (paramValue) {
+            return encodeURIComponent(paramName) + '=' + encodeURIComponent(paramValue)
+        });
+        return previousEncodedParams.concat(encodedParam);
+    }, []);
+    return url + '?' + encodedParams.join('&');
+};
+
+var base_url = 'https://jmorel.opendatasoft.com/api';
+
+new Vue({
+    el: '#app',
+    components: {
+        'designer-index': {
+            template: '' +
+            '<div class="designer-index">' +
+            '    <div class="designer-index-section"' +
+            '         v-for="(designers, letter) in designerIndex">' +
+            '        <div class="designer-index-section-title">{{ letter }}</div>' +
+            '        <ul class="designer-index-designers">' +
+            '            <li class="designer" v-for="designer in designers"><a :href="designer">{{ designer }}</a></li>' +
+            '        </ul>' +
+            '    </div>' +
+            '</div>',
+            data: function () {
+                // init designer index
+                var emptyDesignerIndex = 'abcdefghijklmnopqrstuvwxyz'.split('').reduce(function (object, letter) {
+                    object[letter] = [];
+                    return object;
+                }, {});
+                var that = this;
+                // fetch remote data
+                fetch(buildGetUrl(base_url + '/records/1.0/analyze/', {
+                    'dataset': 'of-designers-and-chairs',
+                    'x': ['last_name', 'full_name'],
+                    'sort': 'x.last_name,x.full_name',
+                    'y.serie1-1.func': 'COUNT'
+                }), { method: 'GET' })
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function (designers) {
+                        designers.forEach(function (designer) {
+                            that.designerIndex[designer.x.last_name[0].toLowerCase()].push(designer.x.full_name);
+                        });
+                    });
+                return {
+                    designerIndex: emptyDesignerIndex
+                }
+            }
+        }
+    }
+});
+
+// Load designer index
+
+
+
+// Navigation
+
 var toggleElement = document.getElementsByClassName("nav-toggle")[0];
 var navElement = document.getElementsByTagName("nav")[0];
-// var currentDesignerElement = document.getElementsByClassName("current-designer")[0];
 
 toggleElement.addEventListener('click', function (event) {
+    console.log('click');
     if (navElement.classList.contains('expanded')) {
         navElement.classList.remove('expanded');
     } else {
         navElement.classList.add('expanded');
     }
 });
+
